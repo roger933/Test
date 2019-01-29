5f87ad6dcfecf096eb9fe152a1be0b381249e6f8
Roger <rogerclaraso933@gmail.com>
2019-01-29 17:33:59 +0100

    canvis2

---
+++ simple_calculator.py
@@ -0,0 +1,43 @@
+def suma (num1,num2):
+
+    resultado = num1+num2
+    print ("El resultado es " ,resultado)
+
+def resta (num1,num2):
+
+    resultado = num1-num2
+    print ("El resultado es " ,resultado)
+
+def multiplicar (num1,num2):
+
+    resultado = num1*num2
+    print ("El resultado es " ,resultado)
+
+
+print ("Calculadora")
+print ("Elige una opcion: ")
+print ("1. Sumar")
+print ("2. Restar")
+print ("3. Multiplicar")
+
+opcion = input ("Seleccionar una opcion ")
+
+if opcion == "1":
+    num1 = int(input("Introduce el primer numero "))
+    num2 = int(input("Introduce el segundo numero "))
+    suma (num1,num2)
+    
+if opcion == "2":
+    num1 = int(input("Introduce el primer numero "))
+    num2 = int(input("Introduce el segundo numero "))
+    resta (num1,num2)
+
+if opcion == "3":
+    num1 = int(input("Introduce el primer numero "))
+    num2 = int(input("Introduce el segundo numero "))
+    multiplicar (num1,num2)
+    
+
+
+    
+    
